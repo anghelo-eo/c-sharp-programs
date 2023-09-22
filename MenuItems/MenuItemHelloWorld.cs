939625e7004fb3e62b075c56c328a736d78e3a0e
Aleksei Proshkin <alexpro190504@gmail.com>
2023-09-22 11:03:38 +0600

    New version of the menu item 1

--- MenuItems/MenuItemHelloWorld.cs
+++ MenuItems/MenuItemHelloWorld.cs
@@ -7,11 +7,39 @@ namespace Ex01.MenuItems
 {
     public class MenuItemHelloWorld : MenuItemCore
     {
+        //public override string Title { get {return "Hello world!";} }
+        public static string HELLO_WORLD = "Hello world!";
+        public string _HelloWorld = "Hello world!"; 
         //public override string Title { get {return "Привет, мир!";} }
-        public override string Title { get {return "Hello world!";} }
+        public override string Title {
+            get 
+            {
+                if (_HelloWorld == null)
+                {
+                    _HelloWorld = HELLO_WORLD;
+                }
+                return _HelloWorld;
+            }
+
+             
+        }
+
+        public void setTitle(string value)
+        {
+            if (value == null)
+            {
+                _HelloWorld = HELLO_WORLD;
+            }
+
+            else
+            {
+                _HelloWorld = value;
+            }
+        }
 
         public override void Execute()
         {
+            this.setTitle ("New Hello world!"); 
             Console.WriteLine("Hello world!");
             //Console.WriteLine("Привет, мир!");
         }
