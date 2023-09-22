87c8e7baa6b7a343ded6b4f4e400b24052668f20
Aleksei Proshkin <alexpro190504@gmail.com>
2023-09-22 10:42:06 +0600

    added date processing

--- IOUtils.cs
+++ IOUtils.cs
@@ -25,5 +25,24 @@ namespace Ex01
                 Console.WriteLine("ERROR: Incorrect format. Enter Integer value...");
             }
         }
+
+        public static DateTime SafeReadDate(string message)
+        {
+            if(!string.IsNullOrEmpty(message))
+            {
+                Console.WriteLine(message);
+            }
+            while(true)
+            {
+                string sValue = Console.ReadLine();
+                DateTime date;
+                if (DateTime.TryParse(sValue, out date))
+                {
+                    return date;
+                }
+                //Console.WriteLine("Ошибка ввода. Введите дату и время корректно...");
+                Console.WriteLine("ERROR: Incorrect format. Enter correct date time...");
+            }
+        }
     }
 }
