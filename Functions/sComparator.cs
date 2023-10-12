ad76e6d24d138b2c59e4c5aa89d246ef5df9ea6f
Aleksei Proshkin <alexpro190504@gmail.com>
2023-10-12 21:58:56 +0600

    added string comparison

---
+++ Functions/sComparator.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Ex01.MenuItems
+{
+    public class sComparator : MenuItemRecursionDate
+    {
+        public static int Compar(string sOne, string sTwo, int sOneLen, int sTwoLen)
+        {
+            var count = -1;
+            if (sOne.Take(sTwoLen).All(e => sOne[++count] == sTwo[count]))
+            {
+                ++count;
+            }
+            return count;
+        }
+    }
+}
--- MenuItems/MenuItemStrings.cs
+++ MenuItems/MenuItemStrings.cs
@@ -12,12 +12,37 @@ namespace Ex01.MenuItems
 
         public override void Execute()
         {
-            Console.Write("Enter the first string with fewer than 20 characters:");
-            string? stringone = Console.ReadLine();
-            Console.WriteLine($"String is {stringone}");
-            Console.Write("Enter the second string with fewer than 20 characters:");
-            string? stringtwo = Console.ReadLine();
-            Console.WriteLine($"String is {stringtwo}");
+            string? sOne = "";
+            string? sTwo = "";
+            Console.Write("Enter the first string with fewer than 40 characters:");
+            sOne = Console.ReadLine();
+            int sOneLen = sOne.Length;
+            //Console.WriteLine($"String is {sOne}");
+            Console.WriteLine(sOneLen);
+            if (sOneLen > 39)
+            {
+                Console.WriteLine("ERROR: The entered string is longer than 40 characters.");
+                Execute();
+            }
+            Console.Write("Enter the second string with fewer than 40 characters:");
+            sTwo = Console.ReadLine();
+            int sTwoLen = sTwo.Length;
+            Console.WriteLine(sTwoLen);
+            if (sTwoLen > 39)
+            {
+                Console.WriteLine("ERROR: The entered string is longer than 40 characters.");
+                Execute();
+            }
+            //Console.WriteLine($"String is {sTwo}");
+            int CompRes = sComparator.Compar(sOne, sTwo, sOneLen, sTwoLen);
+            if (sOneLen == sTwoLen)
+            {
+                if (sOneLen == CompRes)
+                {
+                    Console.WriteLine("Strings are equal characters by characters.");
+                }
+            }
+            Console.WriteLine("Number of matching characters is :{0}", CompRes);
         }
     }
 }
